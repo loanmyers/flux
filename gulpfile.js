82aa1a6343f9d7a044b2b85e54f903f1b4732ae7
Loan Myers <loan.myers@gmail.com>
2015-10-09 13:24:29 +0100

    editing gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -4,19 +4,19 @@ var reactify = require('reactify');
 var source = require('vinyl-source-stream');
 
 gulp.task('browserify', function() {
-	browserify('./src/main/js')
+	browserify('./src/js/main.js')
 	.transform('reactify')
 	.bundle()
 	.pipe(source('main.js'))
-	pipe(gulp.dest('dist/js'));
+	.pipe(gulp.dest('dist/js'));
 });
 
-gulp.task('copy'), function () {
+gulp.task('copy'	, function() {
 	gulp.src('src/index.html')
-		.pip(gulp.dest('dist'));
+		.pipe(gulp.dest('dist'));
 	gulp.src('src/assets/**/*.*')
 		.pipe(gulp.dest('dist/assets'));
-}
+});
 
 gulp.task('default', ['browserify', 'copy'], function() {
 	return gulp.watch('src/**/*.*', ['browserify', 'copy'])
