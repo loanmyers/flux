6211aded1c23ec38e94aa44cd2d32315b0f9bf23
Loan Myers <loan.myers@gmail.com>
2016-02-27 10:58:03 +0000

    created actions handler

--- src/js/dispatchers/app-dispatcher.js
+++ src/js/dispatchers/app-dispatcher.js
@@ -2,9 +2,10 @@ var Dispatcher = require('flux').Dispatcher;
 var assign = require('react/lib/Object.assign');
 
 var AppDispatcher = assign(new Dispatcher(), {
-	handleViewAction: funtion(action){
+	handleViewAction: function(action){
+		console.log('action', action)
 		this.dispatch({
-			source: 'VIEW_ITEM',
+			source: 'VIEW_ACTION',
 			action: action
 		})
 	}
