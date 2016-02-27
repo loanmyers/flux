307e49acd359f2962721c74e46674b875c66229f
Loan Myers <loan.myers@gmail.com>
2016-02-27 10:57:53 +0000

    created actions handler

--- src/js/actions/app-actions.js
+++ src/js/actions/app-actions.js
@@ -3,9 +3,29 @@ var AppDispatcher = require('../dispatchers/app-dispatcher');
 
 var AppActions = {
 	addItem: function(item){
+        AppDispatcher.handleViewAction()
 		AppDispatcher.handleViewAction({
-			actionType: AppConstants.ADD_ITEM,
+            actionType: AppConstants.ADD_ITEM,
+            item: item })
+	},
+	removeItem: function(index){
+		AppDispatcher.handleViewAction({
+			actionType: AppConstants.REMOVE_ITEM,
+			index: index
+		})
+	},
+	increaseItem: function(index){
+		AppDispatcher.handleViewAction({
+            actionType: AppConstants.INCREASE_ITEM,
+			index: index
+		})
+	},
+	decreaseItem: function(index){
+		AppDispatcher.handleViewAction({
+            actionType: AppConstants.DECREASE_ITEM,
+            index: index
 		})
 	}
 }
 
+module.exports = AppActions;
