79696cb72da9c841cc778927fdf7fe7970cb89ec
Loan Myers <loan.myers@gmail.com>
2016-08-23 20:36:17 +0200

    update sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,26 @@
-var gulp = require('gulp');
-var browserify = require('browserify');
-var reactify = require('reactify');
-var source = require('vinyl-source-stream');
+var gulp = require('gulp'),
+    browserify = require('browserify'),
+    reactify = require('reactify'),
+    source = require('vinyl-source-stream');
 
 gulp.task('browserify', function() {
 	browserify('./src/js/main.js')
-	.transform('reactify')
-	.bundle()
-	.pipe(source('main.js'))
-	.pipe(gulp.dest('dist/js'));
+        .transform('reactify')
+        .bundle()
+        .pipe(source('main.js'))
+        .pipe(gulp.dest('dist/js'));
 });
 
 gulp.task('copy', function() {
-	gulp.src('src/index.html')
+	gulp
+        .src('src/index.html')
 		.pipe(gulp.dest('dist'));
-	gulp.src('src/assets/**/*.*')
+	gulp
+        .src('src/assets/**/*.*')
 		.pipe(gulp.dest('dist/assets'));
 });
 
 gulp.task('default', ['browserify', 'copy'], function() {
-	return gulp.watch('src/**/*.*', ['browserify', 'copy'])
+	return gulp
+        .watch('src/**/*.*', ['browserify', 'copy'])
 });
